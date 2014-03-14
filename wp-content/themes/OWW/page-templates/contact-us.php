3fe9e2facabc2319976f6ad084e40bb0a408df5b
Melanie27 <melaniemcganney@gmail.com>
2014-03-14 11:15:38 -0700

    new page templates
    contact us, privacy policy

---
+++ wp-content/themes/OWW/page-templates/contact-us.php
@@ -0,0 +1,124 @@
+<?php
+/**
+
+ * Template Name: Contact Us*/
+
+?>
+
+<?php get_header(2); ?>
+			
+			
+<body id="contact-us">
+  	<div class="container-full">
+  		<section class="container landing">
+		 	<?php include (TEMPLATEPATH . '/_/components/php/header-menu.php'); ?>	
+			
+		  		<section class="two">
+					<div class="row heading">
+						<section class="col-lg-12">
+							<div class="container">
+								<h1>Contact Us</h1>
+							</div>
+						</section>		
+					</div><!--heading-->		
+				<!--place Hero overlay here-->
+		  		</section> <!--two container-->	
+				<section class="three container">
+					<div class="row over2">
+						<section class="reservations">
+							<h1>Reservations, Corporate Sales and Customer Service</h1>
+							<ul class="contact-numbers">
+								<li>Phone Number: <span class="click-number"><a href="tel:+18773887041">1-877-388-7041</a></span></li>
+								<li>International: <span class="click-number"><a href="tel:+16024272781">1-602-427-2781</a></span></li>
+							</ul>
+						</section>
+						<section class="headquarters">
+							<h1>Worldwide Headquarters</h1>
+							<div class="address">
+								<p>Oakwood Worldwide</p>
+								<p>2222 Corinth Avenue</p>
+								<p>Los Angeles, CA 90064</p>
+								<p>United States</p><br/><br/>
+								<p>Phone: <span class="click-number"><a href="tel:+18773887041">1-877-388-7041</a></span></p>
+								<p>International: <span class="click-number"><a href="tel:+16024272781">1-602-427-2781</a></span></p>
+								<p><a href="#" class="submit-request">Submit a Housing Request</a></p>
+
+							</div>	
+						</section>
+						<section class="regional-headquarters">
+							<h1>Regional Headquarters</h1>
+							<ul class="region-list">
+								<li class="col-lg-3 col-xs-12">
+									<h3>United States and Canada</h3>
+									<div class="address">
+										<p>Oakwood Worldwide Sales and Service Center (WSSC)</p>
+										<p>9630 N. 25th Avenue, Suite 300 </p>
+										<p>Phoenix, AZ 85021</p>
+										<p>United States</p><br/><br/>
+										<p>Phone: <span class="click-number"><a href="tel:+18773887041">1-877-388-7041</a></span></p>
+										<p>International: <span class="click-number"><a href="tel:+16024272781">1-602-427-2781</a></span></p>
+										<p><a href="#" class="submit-request">Submit a Housing Request</a></p>
+									</div>	
+								</li>
+								<li class="col-lg-3 col-xs-12">
+									<h3>Europe, Middle East and Africa</h3>
+									<div class="address">
+										<p>Oakwood United Kingdom</p>
+										<p>40 Clifton Street </p>
+										<p>London EC2A 4DX</p>
+										<p>United Kingdom</p><br/><br/>
+										<p>Phone: <span class="click-number"><a href="tel:+4402077494460">+44 020 7749 4460 </a></span></p>
+				
+										<p>Email: <a href="mailto:oakwooduk@oakwood.com" class="email">oakwooduk@oakwood.com</a></p>
+									</div>	
+								</li>
+								<li class="col-lg-3 col-xs-12">
+									<h3>South America</h3>
+									<div class="address">
+										<p>Oakwood Worldwide Sales and Service Center (WSSC)</p>
+										<p>9630 N. 25th Avenue, Suite 300 </p>
+										<p>Phoenix, AZ 85021</p>
+										<p>United States</p><br/><br/>
+										<p>Phone: <span class="click-number"><a href="tel:+18773887041">1-877-388-7041</a></span></p>
+										<p>International: <span class="click-number"><a href="tel:+16024272781">1-602-427-2781</a></span></p>
+										<p><a href="#" class="submit-request">Submit a Housing Request</a></p>
+									</div>	
+								</li>
+								<li class="col-lg-3 col-xs-12">
+									<h3>Asia Pacific</h3>
+									<div class="address">
+										<p>APAC Global Sales Office</p>
+										<p>101 Thomson Road, #26-03, United Square</p>
+										<p>Singapore 307591</p><br/><br/>
+										<p>Phone: <span class="click-number"><a href="tel:+6565216380">+65 6521 6380</a></span></p>
+										<p>Email: <a href="mailto:globalsolutions_apac@oakwood.com" class="submit-request">globalsolutions_apac@oakwood.com</a></p><br/><br/>
+
+										<p>Oakwood Singapore</p>
+										<p>200 Newton Road #04-01 Newton 200</p>
+										<p>Singapore 307983 </p><br/><br/>
+										<p>Phone: <span class="click-number"><a href="tel:+6563366900">+65 6336 6900</a></span></p>
+										<p>Email: <a href="mailto:sales.oap@oakwoodasia.com" class="submit-request">sales.oap@oakwoodasia.com</a></p>
+									</div>	
+								</li>
+							</ul>	
+						</section>	
+						<div class="clear"></div>	
+						
+					</div><!--over2-->
+				</section><!--end three-->	
+				<section class="row hidden-lg hidden-md hidden-sm hidden-xs">
+					<div class="col-lg-12">
+						<section class="four">
+						</section>
+					</div>
+				</section><!--content 4-->		
+				
+					<section class="row hidden-xs">
+						<div class="col-lg-12">
+							<section class="six">
+							</section>
+						</div>
+					</section><!--content 6-->
+				<hr />
+						
+		<?php get_footer(); ?>
---
+++ wp-content/themes/OWW/page-templates/privacy-policy.php
@@ -0,0 +1,81 @@
+<?php
+/**
+
+ * Template Name: Privacy Policy*/
+
+?>
+
+<?php get_header(2); ?>
+			
+			
+<body id="privacy-policy">
+  	<div class="container-full">
+  		<section class="container landing">
+		 	<?php include (TEMPLATEPATH . '/_/components/php/header-menu.php'); ?>	
+			
+		  		<section class="two">
+					<div class="row heading">
+						<section class="col-lg-12">
+							<div class="container">
+								<h1>Privacy Policy</h1>
+							</div>
+						</section>		
+					</div><!--heading-->		
+				<!--place Hero overlay here-->
+		  		</section> <!--two container-->	
+				<section class="three container">
+					<div class="row over2">
+						<section class="col-lg-12 col-md-12 privacy-intro">
+							<div class="row">
+								<section class="col-lg-6 col-md-6 col-xs-12">
+									<h3>Online Privacy Statement</h3>
+									<p>(Last Updated August 21, 2013)</p>
+									<div class="download-pdf"><a href="#" class="download-link">Download PDF</a></div>
+								</section>	
+								<section class="col-lg-3 col-md-3 col-xs-12 etrust-swiss">Swiss</section>
+								<section class="col-lg-3 col-md-3 col-xs-12 etrust-eu">EU</section>
+							</div>
+						</section>
+						<section class="col-lg-12 privacy-text">
+							<p>We are dedicated to protecting your privacy and safeguarding your "personally identifiable" information. We are committed to using our customers' information only in a responsible manner. We are committed to respecting your privacy and adhering to the principles of applicable data protection and privacy laws.</p>
+							<p>This Online Privacy Statement is an explanation of the information we collect, how we use it, and how the use of this information can benefit your experience on our websites and during your relationship with us. The information outlined below will assist you in making informed decisions while doing business with us.</p>
+							<p>In order to provide multiple access points to the services and products we offer, R&B Realty, a California Ltd. Partnership DBA Oakwood Worldwide operates directly, or through its affiliates, subsidiaries, or service providers, many websites, including <a href="http://oakwood.com" target="_blank">www.oakwood.com</a>, <a href="http:execustay.com" target="_blank">www.execustay.com</a>, <a href="http://childactors.net" target="_blank">www.childactors.net</a>, <a href="http://oakwoodworldwide.com" target="_blank">www.oakwoodnewsletter.com</a> and other websites listed here (individually, "Website" and collectively, "Websites".)</p>
+						</section>
+						<section class="col-lg-12 method-text">
+							<h3>Method of Collection of Informaiton</h3>
+							<p>Oakwood Worldwide does not collect personally identifiable information when you browse the Oakwood Worldwide Websites unless you voluntarily and knowingly provide such information. This means that we will not know your name, your email address, or any other personally identifiable information just because you browse the Oakwood Worldwide Websites unless you:</p>
+							<ul>	
+								<li>access a Website from a link in an email that we sent;</li>
+								<li>are a myOakwood, eOakwood or eConnectivity member and log-in to your account; or</li>
+								<li>have created a profile and log-in when you access a Website.</li>
+							</ul>
+							<p>When we do need to collect personally identifiable information from you, we will ask you, your employer, your relocation company or travel agency to voluntarily supply us with the information we need. For example, if you would like to make a reservation at one of our properties, we will ask you for information such as name, address, telephone number, email address, credit card number and room preference to process your reservation. We may collect this information directly from you by telephone, fax, email, at the counter, upon completion of an application or enrollment form, or through your use of the Websites.</p>
+							<p>Information may also be collected as part of: (i) fulfilling reservation or information requests, (ii) registering for myOakwood program membership, (iii) submitting a job application, (iv) submitting a credit application, or (v) responding to communications from us or our affiliates (e.g., surveys, promotional offers, or reservation confirmations), (vi) registering for, or responding to a subscription request for our newsletter. We endeavor to collect information only with your knowledge and with your permission if necessary.</p>
+						</section>
+						<section class="col-lg-12 types-text">
+							<h3>Types Of Information</h3>
+							<p>The types of personally identifiable information that we collect may include your name, home, work and email addresses, telephone and fax numbers, credit card information, date of birth, gender, salary, social security number (in limited instances), password and security responses, and lifestyle information, such as room preferences, leisure activities, and other information necessary to fulfill special requests (e.g., health conditions that require special room accommodations).</p>
+							<p>Oakwood Worldwide may also collect non-personally identifiable information about you, such as your use of our websites, communication preferences, travel habits, aggregated data relative to your stays, and responses to promotional offers and surveys.</p>	
+							<p>You may be offered the choice not to submit your personal information or to decide what communications you want to receive from us. However, doing so may cause certain transactions to become affected. For example, failure to provide your name will prevent the processing of reservations.</p>
+						</section>	
+						
+						<div class="clear"></div>	
+						
+					</div><!--over2-->
+				</section><!--end three-->	
+				<section class="row hidden-lg hidden-md hidden-sm hidden-xs">
+					<div class="col-lg-12">
+						<section class="four">
+						</section>
+					</div>
+				</section><!--content 4-->		
+				
+					<section class="row hidden-xs">
+						<div class="col-lg-12">
+							<section class="six">
+							</section>
+						</div>
+					</section><!--content 6-->
+				<hr />
+						
+		<?php get_footer(); ?>
