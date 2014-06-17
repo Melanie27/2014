6ea6043203afc485cdc156c27fe8badd272ec9db
Melanie27 <melaniemcganney@gmail.com>
2014-06-17 09:57:44 -0700

    more

---
+++ wp-content/themes/OWW/page-templates/global-scale.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * * Template Name: Global Scale
+ 
+ */
+
+get_header(3); ?>
+<body id="global-scale">
+  	<div class="container-full">
+  		<section class="container landing">
+		 	<?php include (TEMPLATEPATH . '/_/components/php/header-menu.php'); ?>	
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+
+			<section class="two">
+			 <div class="row heading">
+				 <section class="col-lg-12">
+					 <div class="container">
+						 <h1>Global Scale & Inventory</h1>										
+					</div>
+				</section>		
+			</div><!--heading-->	
+		  </section> <!--two container-->							
+		  <section class="three container">
+			<div class="row">
+				<section class="col-lg-12 col-md-12 global-intro">
+					<h2>Our Industry-Leading Inventory</h2>
+					<h3>We’re committed to finding the right solutions for our clients and guests</h3>
+					<p>In addition to the inventory of properties we manage, Oakwood Worldwide has invested in an extensive supply chain of over 1,300 housing providers. Through this network we offer access to the world’s largest inventory of corporate housing and serviced apartments, allowing us to provide you with a virtually unlimited selection of accommodations in more than 70 countries and in all 50 states. Our unmatched global footprint, paired with our dedication to local market expertise gives us the flexibility to create responsive solutions that meet the demands of businesses and business travelers around the world.</p>
+				</section>	
+			</div>
+		</section>
+		
+		<section class="three three-a container">
+				<div class="row">
+					<section class="col-lg-12 col-md-12 global-reach">
+						<h2>United States & Canada</h2>
+					</section>
+				</div>				
+				<div class="row">
+					<section class="col-lg-12 col-md-12">						
+						<div class="row">
+							<section class="col-lg-4 col-md-4 col-sm-6 col-xs-12">		
+								<div class="solutions">
+									<a href="#" class="industry-sections">
+										<img src="<?php bloginfo( 'template_directory' ); ?>/images/global-services/global-reach.jpg" alt="global-reach" class="img-responsive">
+									</a>
+								</div>
+							</section>
+							<section class="col-lg-8 col-md-8 col-sm-6 global-reach">
+								<h3>Discover Popular Destinations in the U.S. and Canada</h3>
+								<p>Oakwood Worldwide offers prime locations in urban and suburban neighborhoods in all 50 states and over 22 Canadian cities. Plus, with global headquarters in Los Angeles, a 24-hour worldwide sales and service center in Phoenix and dozens of regional offices in the U.S and Canada, we know the environs and can place your guests in the accommodations that fit their unique needs.</p>
+							</section>
+						</div><!--end nested row-->	
+					</section>		
+				</div>			
+			</section><!--three-->
+			<hr/>
+			<section class="three three-a container">
+				<div class="row">
+					<section class="col-lg-12 col-md-12 global-reach">
+						<h2>South America</h2>
+					</section>
+				</div>				
+				<div class="row">
+					<section class="col-lg-12 col-md-12">						
+						<div class="row">
+							<section class="col-lg-4 col-md-4 col-sm-6 col-xs-12">		
+								<div class="solutions">
+									<a href="#" class="industry-sections">
+										<img src="<?php bloginfo( 'template_directory' ); ?>/images/global-services/global-reach.jpg" alt="global-reach" class="img-responsive">
+									</a>
+								</div>
+							</section>
+							<section class="col-lg-8 col-md-8 col-sm-6 global-reach">
+								<h3>Discover Popular South America Destinations</h3>
+								<p>As more companies expand their scope to include South America, they look to Oakwood Worldwide for innovative housing solutions and industry-leading local market expertise. Offering a wide range of corporate housing and serviced apartments across Chile, Peru, Brazil, Mexico, Argentina, Costa Rica and Venezuela, we have the ability to provide guests with accommodations that make them comfortable in their environs and focused on the business at hand in this growing region of the world.</p>	
+							</section>
+						</div><!--end nested row-->	
+					</section>		
+				</div>			
+			</section><!--three-->	
+			
+			<section class="three three-a container">
+				<div class="row">
+					<section class="col-lg-12 col-md-12 global-reach">
+						<h2>EMEA</h2>
+					</section>
+				</div>				
+				<div class="row">
+					<section class="col-lg-12 col-md-12">						
+						<div class="row">
+							<section class="col-lg-4 col-md-4 col-sm-6 col-xs-12">		
+								<div class="solutions">
+									<a href="#" class="industry-sections">
+										<img src="<?php bloginfo( 'template_directory' ); ?>/images/global-services/global-reach.jpg" alt="global-reach" class="img-responsive">
+									</a>
+								</div>
+							</section>
+							<section class="col-lg-8 col-md-8 col-sm-6 global-reach">
+								<h3>Discover Popular Destinations in EMEA</h3>
+								<p>With regional headquarters in London and access to hundreds of properties in 60 cities across the United Kingdom, Oakwood Worldwide offers the expertise to find corporate housing and serviced apartments that fit any traveler’s specifications. Our team of experts is ready to provide guests the accommodations and support that will position them for business success anywhere in the United Kingdom.
+								<p>In addition to our expertise in the United Kingdom, we offer unparalleled service across all of Europe, with a strong presence in Germany, France, Belgium, Italy, the Netherlands, Sweden and Switzerland, among others.</p>
+								<p>The evolving world of business has seen the remarkable rise of the Middle East and Africa as powerful centers of commerce in recent years. That makes Oakwood Worldwide’s strong presence in the region an important resource for companies seeking to capitalize on the growth of opportunities available there. As the region continues to grow, we continue to invest in new housing solutions throughout this evolving corner of the globe, consistently providing companies and travelers with innovative solutions that keep them ready for the next initiative.</p>
+								<a class="orange-link" href="risk-management-methods">Learn More &#8594;</a>	
+							</section>
+						</div><!--end nested row-->	
+					</section>		
+				</div>			
+			</section><!--three-->
+			
+			<section class="three three-a container">
+				<div class="row">
+					<section class="col-lg-12 col-md-12 global-reach">
+						<h2>Risk Management Methods</h2>
+					</section>
+				</div>				
+				<div class="row">
+					<section class="col-lg-12 col-md-12">						
+						<div class="row">
+							<section class="col-lg-4 col-md-4 co
